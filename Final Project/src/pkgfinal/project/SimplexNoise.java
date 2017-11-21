7f0aa29556e025414c9e5c845ffdde0aa8bf38e9
Omar Rodriguez <oerodriguez@cpp.edu>
2017-11-20 22:51:35 -0800

    Adds comments to the modified classes

--- Final Project/src/pkgfinal/project/SimplexNoise.java
+++ Final Project/src/pkgfinal/project/SimplexNoise.java
@@ -1,22 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*******************************************************************************
+* File: SimplexNoise.java 
+* Group: The RenderMen
+* Members:
+*       * Marc Deaso
+*       * Omar Rodriguez
+*       * Nick Spencer
+*       * Luke Walsh
+*       * Alex Winger
+* Class: CS 445: – Computer Graphics 
+* 
+* Assignment: Final Project 
+* Date Last Modified: 11/20/2017
+* 
+* Purpose: A pre-written class that will generate noise so that the Minecraft
+*           world contains natural hills and valleys
+* 
+*******************************************************************************/
 package pkgfinal.project;
 
 import java.util.Random;
 
 public class SimplexNoise {
 
+//  octaves of noise to create fractal noise
     SimplexNoise_octave[] octaves;
+//  frequencies and amplitudes created for the waves that create the noise
     double[] frequencys;
     double[] amplitudes;
 
+//    the values that are used for the constructor in the SimplexNoise class
     int largestFeature;
     double persistence;
     int seed;
 
+//    SimplexNoise constructor
     public SimplexNoise(int largestFeature,double persistence, int seed){
         this.largestFeature=largestFeature;
         this.persistence=persistence;
@@ -25,10 +42,12 @@ public class SimplexNoise {
         //recieves a number (eg 128) and calculates what power of 2 it is (eg 2^7)
         int numberOfOctaves=(int)Math.ceil(Math.log10(largestFeature)/Math.log10(2));
 
+//        creates arrays for the number of octaves, frequencies, and amplitudes
         octaves=new SimplexNoise_octave[numberOfOctaves];
         frequencys=new double[numberOfOctaves];
         amplitudes=new double[numberOfOctaves];
 
+//        creates a random seed
         Random rnd=new Random(seed);
 
         for(int i=0;i<numberOfOctaves;i++){
@@ -44,7 +63,8 @@ public class SimplexNoise {
 
     }
 
-
+//Method: getNoise
+//    Purpose: returns the Noise for a 2D height
     public double getNoise(int x, int y){
 
         double result=0;
@@ -61,6 +81,8 @@ public class SimplexNoise {
 
     }
 
+    //Method: getNoise
+//    Purpose: returns the Noise for a 3D height
     public double getNoise(int x,int y, int z){
 
         double result=0;
